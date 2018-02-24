b29f1cd2589c0c3307f2ea135a8bbf16827f7163
mv84 <marcosv84@gmail.com>
2018-02-24 13:35:37 -0300

    Actualización Módulos
    Agrego los módulos ABB, lista string y Archivo .cpp

---
+++ Lista_string.cpp
@@ -0,0 +1,70 @@
+#include "Lista_string.h"
+
+//Crear lista vacìa
+void Crear_Lista (Lista &L)
+{
+    L = NULL;
+}
+
+//Agregar elemento al inicio
+void InsFront (strings str, Lista &L)
+{
+    Lista aux = new nodoL;
+    strcop(aux->info,str);
+    aux->sig = L;
+    L = aux;
+}
+
+//Insertar nodo al final de la lista
+void InsBack (strings str, Lista &L)
+{
+    if (L == NULL)
+    InsFront(str,L);
+    else
+    InsBack (str, L->sig);
+}
+
+//Saber si una lista està vacìa
+boolean Esta_Vacia (Lista L)
+{
+    return boolean (L == NULL);
+}
+
+//obtener el primer elemento de una lista
+void Primero (Lista L, strings &str)
+{
+    strcop(str,L->info);
+}
+
+// Mostrar lista por pantalla
+void Mostrar_Lista (Lista L)
+{
+    if (L != NULL)
+    {
+        print(L->info);
+        printf("\n");
+        Mostrar_Lista(L->sig);
+    }
+}
+
+//Parseo
+void Parseo (strings str,Lista &parseo)
+{
+    strings aux;
+    strcrear(aux);
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+        {
+            i++;
+        }
+        else
+        {
+            fflush(stdin);
+            scan_palabra(str,aux,i);
+            InsBack(aux,parseo);
+        }
+    }
+    strdestruir(aux);
+}
