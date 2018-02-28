7381f628b3d6b4c0b34eb57d8e7dcdf762ff36e5
marceloalorenzo <32404538+marceloalorenzo@users.noreply.github.com>
2018-02-27 21:11:31 -0300

    prueba main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
+#include "Lista_Instrucciones.h"
+#include "Lista_string.h"
+#include "StringDinamico.h"
+#include<stdio.h>
+#include<stdlib.h>
 
+
+int main ()
+{
+    Lista L; //L Se llama Lista
+    Crear_Lista(L);//Creo nodo
+    strings prueba; //le doy nombre al string
+    strcrear(prueba); //creo string llamado prueba
+
+    {}
+
+    //while ( strings != '\0'){
+
+
+
+      //                      }
+
+//    crear(a);//Creo Arbol
+//    Arbol a;//a se llama Arbol
+
+
+
+}
