cf2df89578e586cb6f38c5ea2a59c3cb973b70c2
marceloalorenzo <32404538+marceloalorenzo@users.noreply.github.com>
2018-03-05 19:02:31 -0300

    Ejecutar

---
+++ Ejecutar.cpp
@@ -0,0 +1,15 @@
+#include "Ejecutar.h"
+
+void Ejecutar (strings str)
+
+
+{
+    //Leo la palabra despues de la palabra comando
+    //Si hay mas de una palabra mando error
+    strcrear(compilado)
+    strings compilado;
+
+
+
+}
+
