837d913cd3d6e9c1d68d805b3e60b9d6a3c8ce97
mv84 <marcosv84@gmail.com>
2018-03-07 03:14:53 -0300

    Ejecutar
    Ejecutar

--- Ejecutar.cpp
+++ Ejecutar.cpp
@@ -2,122 +2,104 @@
 
 
 void Ejecutar (strings str)
-
-
 {
     ListaStrings listaEjecucion;
-    strings auxVARS;
-    strcrear(auxVARS);
-    strcop(auxVARS,str);
-    strcon(auxVARS,VARS);
-    strings auxINST;
-    strcrear (auxINST);
-    strcop (auxINST,str);
-    strcon (auxINST, INST);
+
+    strings auxVars;
+    strcrear(auxVars);
+    strcop(auxVars,str);
+    strcon(auxVars,VARS);
+
+    strings auxInst;
+    strcrear (auxInst);
+    strcop (auxInst,str);
+    strcon (auxInst,INST);
 
 
+    if (ExisteArchivo(auxVars) && ExisteArchivo(auxInst)) //veo si existen los dos archivos con las extensiones solicitadas
+    {
+     ArbolVariables variables;
+     ListaInstrucciones instrucciones;
+     crearABBvariable(variables);
+     CrearListaInst(instrucciones);
 
-    if ExisteArchivo(auxVARS)&&(auxINST) //veo si existen los dos archivos con las extensiones solicitadas
+     LevantarAbb(variables,auxVars);
+     LevantarLista(instrucciones,auxInst);
+
+     boolean errorEjecucion = FALSE;
+
+        while ( variables != NULL && errorEjecucion != TRUE) //mientras la liesta no sea nula ingreso al nodo
         {
-         ArbolVariables variables;
-         ListaInstrucciones instrucciones;
-         crearABBvariable(variables);
-         CrearListaInst(instrucciones);
-         LevantarAbb(auxVARS);
-         LevantarLista(instrucciones);
-
-            while ( variables != NULL) //mientras la liesta no sea nula ingreso al nodo
+            tipoInstruccion tipoFunc = darTipoInst(instrucciones->info);
+            switch (tipoFunc)
             {
-
-                switch (dar_tipo_inst(variables->info))
-              //obtengo el dato cargado en el discriminante
-                case LEER:  //Obtengo el nombre de la variable guardada en la compilación
-                                {//agregar control con while
-                                    int lectura;
-                                    printf("\nIngrese un numero entero: ");
-                                    scanf("%d", &lectura);
-                                    CargarValorVariable(variables,instrucciones->info.datoDisc.variable1,lectura);//
-                                }
-                                break;
-                case MOSTRAR:
+          //obtengo el dato cargado en el discriminante
+            case LEER:  //Obtengo el nombre de la variable guardada en la compilación
+                            {//agregar control con while
+                                int lectura;
+                                printf("\nIngrese un numero entero: ");
+                                scanf("%d", &lectura);
+                                CargarValorVariable(variables,instrucciones->info.datoDisc.variable1,lectura);//
+                            }
+                break;
+            case MOSTRAR:
+                            {
+                                MostrarValorVariable(variables,instrucciones->info.datoDisc.variable2);
+                            }
+                break;
+            case ASIGVAL:
+                            {
+                                CargarValorVariable(variables,instrucciones->info.datoDisc.asig1.varAsig,instrucciones->info.datoDisc.asig1.valAsig);
+                            }
+                break;
+            case ASIGVAR:
+                            {
+                                int valor = darValorVariable(variables,instrucciones->info.datoDisc.asig2.var2);
+                                CargarValorVariable(variables, instrucciones->info.datoDisc.asig2.var1,valor);
+                            }
+                break;
+            case FUNCVALVAL:
+                            {
+                                int resultado = EjecutarFuncValVal(instrucciones->info);
+                                CargarValorVariable(variables,instrucciones->info.datoDisc.asig3.varAsig,resultado);
+                            }
+                break;
+            case FUNCVARVAR:
+                            {
+                                int resultado;
+                                EjecutarFuncVarVar(instrucciones->info,variables,errorEjecucion,resultado);
+                                if (errorEjecucion != TRUE)
                                 {
-                                    MostrarValorVariable(variables,instrucciones->info.datoDisc.variable2);
-
+                                    CargarValorVariable(variables,instrucciones->info.datoDisc.asig4.variable,resultado);
                                 }
-                                break;
-                case ASIGVAL:
+                            }
+                break;
+            case FUNCVARVAL:
+                            {
+                                int resultado = EjecutarFuncVarVal(instrucciones->info,variables);
+                                CargarValorVariable(variables,instrucciones->info.datoDisc.asig5.variable,resultado);
+                            }
+                break;
+            case FUNCVALVAR:
+                            {
+                                int resultado;
+                                EjecutarFuncVarVar(instrucciones->info,variables,errorEjecucion,resultado);
+                                if (errorEjecucion != TRUE)
                                 {
-                                    CargarValorVariable(variables,instrucciones->info.datoDisc.asig1.varAsig,instrucciones->info.datoDisc.asig1.valAsig);
-
-
+                                    CargarValorVariable(variables,instrucciones->info.datoDisc.asig6.variable,resultado);
                                 }
-                                break;
-                case ASIGVAR:
-                                {
-                                CargarValorVariable(variables,instrucciones->info.datoDisc.asig2.var1,instrucciones->info.datoDisc.asig2.var2);
-                                }
-                                break;
-                case FUNCVALVAL:
-                                {
-                                //segun tipo de funcion
-                                //
-                                switch(tipoFuncion)
-                                case SUM:
-                                            {
-                                                Obtengo datos guardados en param1 y param2
-                                                Realizo la suma de param1 + param2
-                                                Guardo el valor en un auxiliar
-                                                Obtengo el nombre de la variable del campo variable
-                                                Busco en el ABB la variable
-                                                Guardo en el campo valor de la variable en el ABB el resultado de la suma anterior
-
-                                                int auxParam;
-                                                auxParam = param1 + param2;
-                                                darVarAsig3()
-
-                                            }
-                                            break;
-                                case RES:
-                                            {
-                                                Obtengo datos guardados en param1 y param2
-                                                Realizo la resta de param1 - param2
-                                                Guardo el valor en un auxiliar
-                                                Obtengo el nombre de la variable del campo variable
-                                                Busco en el ABB la variable
-                                                Guardo en el campo valor de la variable en el ABB el resultado de la suma anterior
-                                            }
-                                            break;
-                                case MUL:
-                                            {
-                                                Obtengo datos guardados en param1 y param2
-                                                Realizo la multiplicación de param1 * param2
-                                                Guardo el valor en un auxiliar
-                                                Obtengo el nombre de la variable del campo variable
-                                                Busco en el ABB la variable
-                                                Guardo en el campo valor de la variable en el ABB el resultado de la suma anterior
-                                            }
-                                            break;
-                                case DIV:
-                                            {
-                                                Obtengo el dato guardado en param2
-
-                                                SI	param2 es igual a cero	ENTONCES
-                                                    Error de ejecución (El segundo término no puede ser cero)
-                                                    Finalizo ejecución
-                                                SINO
-                                                    Obtengo el dato guardado en param1
-                                                    Realizo la división de param1 / param2
-                                                    Guardo el valor en un auxiliar
-                                                    Obtengo el nombre de la variable del campo variable
-                                                    Busco en el ABB la variable
-                                                    Guardo en el campo valor de la variable en el ABB el resultado de la suma anterior
-                                                FIN SI
-                                            }
-                                            break;
-
-
-                                }
-                                break;
+                            }
+                break;
+            }
+        }
+    }
+    else
+    {
+        printf("\n\nERROR, no existen uno de los archivos .csim o .vars\n\n");
+        system("pause");
+    }
+}
+                                /*
                 case FUNCVARVAR:
                                 {
                                      switch(tipoFuncion)
@@ -285,23 +267,168 @@ void Ejecutar (strings str)
                                                     Guardo en el campo valor de la variable en el ABB el resultado de la suma anterior
                                                 FIN SI
                                             }
-                                            break;
-                                }
-                                break;
-
-            }
-    else
-        {
-        printf("ERROR, no existen uno de los archivos .csim o .vars");
-        }
+                                            break;*/
 
 
 
 
+int EjecutarFuncValVal (instruccion ins)
+{
+    switch (ins.datoDisc.asig3.funcion)
+    {
+        case SUM:
+                    {
+                        int param1 = darParam1Asig3(ins.datoDisc.asig3);
+                        int param2 = darParam2Asig3(ins.datoDisc.asig3);
+                        return (param1 + param2);
+                    }
+            break;
+        case RES:
+                    {
+                        int param1 = darParam1Asig3(ins.datoDisc.asig3);
+                        int param2 = darParam2Asig3(ins.datoDisc.asig3);
+                        return (param1 - param2);
+                    }
+            break;
+        case MUL:
+                    {
+                        int param1 = darParam1Asig3(ins.datoDisc.asig3);
+                        int param2 = darParam2Asig3(ins.datoDisc.asig3);
+                        return (param1 * param2);
+                    }
+            break;
+        case DIV:
+                    {
+                        int param1 = darParam1Asig3(ins.datoDisc.asig3);
+                        int param2 = darParam2Asig3(ins.datoDisc.asig3);
+                        return (param1 / param2);
+                    }
+            break;
+    }
+}
 
+void EjecutarFuncVarVar (instruccion ins, ArbolVariables variables, boolean &errorEjecucion, int &resultado)
+{
+    switch (ins.datoDisc.asig4.funcion)
+    {
+        case SUM:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig4.param1);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig4.param2);
+                        resultado = (param1 + param2);
+                    }
+            break;
+        case RES:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig4.param1);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig4.param2);
+                        resultado = (param1 - param2);
+                    }
+            break;
+        case MUL:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig4.param1);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig4.param2);
+                        resultado = (param1 * param2);
+                    }
+            break;
+        case DIV:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig4.param1);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig4.param2);
+                        if (param2 != 0)
+                        {
+                            resultado = (param1 / param2);
+                        }
+                        else
+                        {
+                            resultado = 0;
+                            errorEjecucion = TRUE;
+                            printf("\n\nError de ejecucion: El parametro 2 en funcion DIV no puede ser cero.\n\n");
+                            system ("pause");
+                        }
+                    }
+            break;
+    }
+}
 
+int EjecutarFuncVarVal (instruccion ins, ArbolVariables variables)
+{
+    switch (ins.datoDisc.asig5.funcion)
+    {
+        case SUM:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig5.param1);
+                        int param2 = darParam2Asig5(ins.datoDisc.asig5);
+                        return (param1 + param2);
+                    }
+            break;
+        case RES:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig5.param1);
+                        int param2 = darParam2Asig5(ins.datoDisc.asig5);
+                        return (param1 - param2);
+                    }
+            break;
+        case MUL:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig5.param1);
+                        int param2 = darParam2Asig5(ins.datoDisc.asig5);
+                        return (param1 * param2);
+                    }
+            break;
+        case DIV:
+                    {
+                        int param1 = darValorVariable(variables,ins.datoDisc.asig5.param1);
+                        int param2 = darParam2Asig5(ins.datoDisc.asig5);
+                        return (param1 / param2);
+                    }
+            break;
+    }
+}
 
-
-
+void EjecutarFuncValVar (instruccion ins, ArbolVariables variables, boolean &errorEjecucion, int &resultado)
+{
+    switch (ins.datoDisc.asig4.funcion)
+    {
+        case SUM:
+                    {
+                        int param1 = darParam1Asig6(ins.datoDisc.asig6);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig6.param2);
+                        resultado = (param1 + param2);
+                    }
+            break;
+        case RES:
+                    {
+                        int param1 = darParam1Asig6(ins.datoDisc.asig6);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig6.param2);
+                        resultado = (param1 - param2);
+                    }
+            break;
+        case MUL:
+                    {
+                        int param1 = darParam1Asig6(ins.datoDisc.asig6);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig6.param2);
+                        resultado = (param1 * param2);
+                    }
+            break;
+        case DIV:
+                    {
+                        int param1 = darParam1Asig6(ins.datoDisc.asig6);
+                        int param2 = darValorVariable(variables,ins.datoDisc.asig6.param2);
+                        if (param2 != 0)
+                        {
+                            resultado = (param1 / param2);
+                        }
+                        else
+                        {
+                            resultado = 0;
+                            errorEjecucion = TRUE;
+                            printf("\n\nError de ejecucion: El parametro 2 en funcion DIV no puede ser cero.\n\n");
+                            system ("pause");
+                        }
+                    }
+            break;
+    }
 }
 
--- Ejecutar.h
+++ Ejecutar.h
@@ -10,4 +10,17 @@ const strings VARS = ".vars";
 
 void Ejecutar (strings str);
 
+// Ejecucion funcion Valor Valor
+int EjecutarFuncValVal (instruccion ins);
+
+// Ejecucion funcion Variable variable
+void EjecutarFuncVarVar (instruccion ins, ArbolVariables variables, boolean &errorEjecucion, int &resultado);
+
+
+// Ejecucion funcion Variable valor
+int EjecutarFuncVarVal (instruccion ins, ArbolVariables variables);
+
+// Ejecucion funcion Valor variable
+void EjecutarFuncValVar (instruccion ins, ArbolVariables variables, boolean &errorEjecucion, int &resultado);
+
 #endif // EJECUTAR_H_INCLUDED
